066d68b9b4de2e5c8530c84385a09c36aac5a71d
Rawlin <rawlinleitao08@gmail.com>
2025-08-15 21:32:22 +0530

    v5

--- client/src/utils/client.js
+++ client/src/utils/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const baseURL =
     process.env.NODE_ENV === "production"
-        ? "https://backend-socialmediaapp.herokuapp.com/api"
+        ? "https://16.171.113.19:8800"
         : "http://localhost:3000";
 
 const client = axios.create({ baseURL });
