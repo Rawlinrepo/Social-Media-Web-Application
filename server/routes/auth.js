d0fe53abb544be02ec2970ff14c7570989a4348d
Rawlin <rawlinleitao08@gmail.com>
2025-08-16 12:41:33 +0530

    v20

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 //REGISTER
 router.post("/register", async (req, res) => {
     try {
+         console.log("Incoming body:", req.body);
         //generate new password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
