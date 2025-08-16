fbdbbfaa5fadf5ec387b13e01696de40368d130e
Rawlin <rawlinleitao08@gmail.com>
2025-08-16 12:11:38 +0530

    v18

--- client/src/utils/client.js
+++ client/src/utils/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const baseURL =
     process.env.NODE_ENV === "production"
-        ? "http://51.20.134.254:8800"
-        : "http://51.20.134.254:8800";
+        ? "http://16.16.182.24/:8800"
+        : "http://16.16.182.24/:8800";
 
 const client = axios.create({ baseURL });
 export default client;
